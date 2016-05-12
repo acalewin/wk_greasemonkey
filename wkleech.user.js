ea946abd22938ea4563979122a95b0aabb7cbc62
Acalewin <acalewin@gmail.com>
2016-05-11 21:04:53 -0400

    create leech script

---
+++ wkleech.user.js
@@ -0,0 +1,23 @@
+
+//@match http://www.wanikani.com/review/session*
+
+if (answerChecker) {
+  var evaluate = answerChecker.evaluate;
+  var answermap = {};
+  answerChecker.evaluate = function(e,t) {
+    var res = evaluate(e,t);
+    if (res.passed == false) {
+      var currItem = $.jStorage.get('currentItem');
+      var k = currItem.id + '-' + e;
+      answermap[k] = answermap[k] ? answermap[k] + 1 : 1;
+      if (answermap[k] >= 3 ) {
+        console.log('Leech item cleared');
+        res['passed'] = true;
+        res['accurate'] = true;
+      }
+    }
+    return res;
+  }
+} else {
+  alert('no checker found');
+}
